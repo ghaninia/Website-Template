4327b1a9471cb39c6dbbb6f95950861e15fa6044
ghaninia <mohamadaminghaninia@gmail.com>
2021-04-24 18:22:45 +0430

    first init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3D4DL</title>
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.rtl.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap-grid.rtl.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap-reboot.rtl.css">
+    <link rel="stylesheet" href="assets/css/bootstrap/bootstrap-utilities.rtl.css">
+
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/fonts.css">
+</head>
+
+<body>
+    <?php include("./master/header.php") ; ?>
+    
+</body>
+</html>
---
+++ master/header.php
@@ -0,0 +1,140 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">
+                <div class="logo">
+                    <a href="">
+                        <h1>3D4DL</h1>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="navbar">
+                    <ul>
+                        <li>
+                            <a href="">محصولات</a>
+                            <div class="childrens">
+                                <div class="child">
+                                    <img src="assets/images/icons/3d.svg" alt="">
+                                    <div class="title">مدل‌های 3 بعدی</div>
+                                    <ol class="columns-2">
+                                        <li>
+                                            <a href="">معماری</a>
+                                        </li>
+                                        <li>
+                                            <a href="">مبلمان</a>
+                                        </li>
+                                        <li>
+                                            <a href="">اتاق کودک</a>
+                                        </li>
+                                        <li>
+                                            <a href="">اتاق کودک</a>
+                                        </li>
+                                        <li>
+                                            <a href="">روشنایی</a>
+                                        </li>
+                                        <li>
+                                            <a href="">دکوری</a>
+                                        </li>
+                                        <li>
+                                            <a href="">صحنه آماده</a>
+                                        </li>
+                                        <li>
+                                            <a href="">صحنه آماده</a>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <a href="">مشاهده همه</a>
+                                    </div>
+                                </div>
+                                <div class="child">
+                                    <img src="assets/images/icons/material.svg" alt="">
+                                    <div class="title">متریال‌ها</div>
+                                    <ol class="columns-2">
+                                        <li>
+                                            <a href="">سنگ</a>
+                                        </li>
+                                        <li>
+                                            <a href="">بتن</a>
+                                        </li>
+                                        <li>
+                                            <a href="">مایع</a>
+                                        </li>
+                                        <li>
+                                            <a href="">آهن</a>
+                                        </li>
+                                        <li>
+                                            <a href="">چوب</a>
+                                        </li>
+                                        <li>
+                                            <a href="">شیشه</a>
+                                        </li>
+                                        <li>
+                                            <a href="">کف</a>
+                                        </li>
+                                        <li>
+                                            <a href="">خاک</a>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <a href="">مشاهده همه</a>
+                                    </div>
+                                </div>
+                                <div class="child">
+                                    <img src="assets/images/icons/learn.svg" alt="">
+                                    <div class="title">آموزش‌ها</div>
+                                    <ol>
+                                        <li>
+                                            <a href="">آموزش مدل سازی 3 بعدی</a>
+                                        </li>
+                                        <li>
+                                            <a href="">آموزش موتور رندر کرونا</a>
+                                        </li>
+                                        <li>
+                                            <a href="">آموزش لومیون</a>
+                                        </li>
+                                        <li>
+                                            <a href="">آموزش پست پرداکشن</a>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <a href="">مشاهده همه</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="">خرید اشتراک ویژه</a>
+                        </li>
+                        <li>
+                            <a href="">آپلود مدل برای فروش</a>
+                        </li>
+                        <li>
+                            <a href="">بلاگ خبری</a>
+                        </li>
+                        <li>
+                            <a href="">ورود به فروشگاه</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="toolbar">
+                    <ul>
+                        <li>
+                            <a href="">ثبت نام</a>
+                        </li>
+                        <li>
+                            <a href="">ورود</a>
+                        </li>
+                    </ul>
+                    <div class="basket">
+                        <a href="" data-unit="0">
+                            <img src="assets/images/icons/basket.svg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
